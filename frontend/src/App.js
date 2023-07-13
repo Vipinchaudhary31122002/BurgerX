7c68e59d653e288c7b2ff065791f32af76259259
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2023-07-13 17:43:56 +0530

    video completed upto 1:56 and i have to work on animation and responsive behaviour or cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
-import Home from "./components/home/Home"
-import Contact from "./components/contact/contact"
+import Home from "./components/home/Home";
+import Contact from "./components/contact/contact";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Cart from "./components/Cart/Cart";
 
 
 // added the scss file
-import "./styles/app.scss"
-import "./styles/header.scss"
-import "./styles/home.scss"
-import "./styles/menu.scss"
-import "./styles/footer.scss"
-import "./styles/contact.scss"
+import "./styles/app.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/menu.scss";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
+import "./styles/cart.scss";
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/contact" element={<Contact/>} />
+      <Route path="/cart" element={<Cart/>} />
     </Routes>
     <Footer/>
   </Router>
