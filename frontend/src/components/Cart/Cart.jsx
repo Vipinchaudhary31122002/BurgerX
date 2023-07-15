90622840104734b5de8418eaf9d2847b05f2d805
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2023-07-15 17:16:52 +0530

    video completed upto 2:03 and i have to work on shipping page

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import burger1 from "../../assets/burger1.png";
 import burger2 from "../../assets/burger2.png";
 import burger3 from "../../assets/burger3.png";
@@ -37,14 +38,34 @@ const Cart = () => {
                     title={"Veg Burger"}
                     img={burger2}
                     value={0}
-                    increment={() => increment(1)}
-                    decrement={() => decrement(1)} />
+                    increment={() => increment(3)}
+                    decrement={() => decrement(2)} />
                 <CartItem
                     title={"Cheese Burger with French Fries"}
                     img={burger3}
                     value={0}
-                    increment={() => increment(1)}
-                    decrement={() => decrement(1)} />
+                    increment={() => increment(3)}
+                    decrement={() => decrement(3)} />
+                <article>
+                    <div>
+                        <h4>Sub Total</h4>
+                        <p>₹{2000}</p>
+                    </div>
+                    <div>
+                        <h4>Tax</h4>
+                        <p>₹{2000 * 0.18}</p>
+                    </div>
+                    <div>
+                        <h4>Delivery Charges</h4>
+                        <p>₹{20}</p>
+                    </div>
+                    <hr />
+                    <div>
+                        <h4>Total </h4>
+                        <p>₹{2000 + 2000 * 0.18 + 20}</p>
+                    </div>
+                    <Link to="/shipping">Checkout</Link>
+                </article>
             </main>
         </section>
     )
